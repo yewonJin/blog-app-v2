961b8948153c5b345e4f2c3804b58c6a873aaf66
yewonJin <jyw966@naver.com>
2024-04-03 16:27:37 +0900

    [UPDATE] 중앙 정렬 및 기본 색상

--- src/shared/ui/Icon/Icon.styled.tsx
+++ src/shared/ui/Icon/Icon.styled.tsx
@@ -5,7 +5,11 @@ import styled from "@emotion/styled";
 const StyledIcon = styled.div`
   width: 30px;
   height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-size: 100%;
+  fill: var(--primaryText);
 
   &:hover {
     cursor: pointer;
