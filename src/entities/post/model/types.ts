cc4b1bddd2a8de5f90fe982adf44af425ababce9
yewonJin <jyw966@naver.com>
2024-04-03 14:39:43 +0900

    [ADD] Post 모델 타입

---
+++ src/entities/post/model/types.ts
@@ -0,0 +1,11 @@
+export type Post = {
+  postNumber: number;
+  title: string;
+  category: string;
+  content: string;
+  keyword: string[];
+  summary: string;
+  date: Date;
+  views: number;
+  hearts: number;
+};
