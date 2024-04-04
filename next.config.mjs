92eb112de695736d9e589ac3a678183026121920
yewonJin <jyw966@naver.com>
2024-04-04 19:58:20 +0900

    [UPDATE domain -> remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["doromobucket.s3.ap-northeast-2.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "doromobucket.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+      },
+    ],
   },
   webpack: (config) => {
     config.module.rules.push({
