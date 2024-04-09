e46f65b7635161c15fe8d0aeeb263cb88a69d0b3
yewonJin <jyw966@naver.com>
2024-04-09 20:25:16 +0900

    [UPDATE] 반응형 레이아웃

--- src/entities/post/ui/PostListItem/PostListItem.styled.tsx
+++ src/entities/post/ui/PostListItem/PostListItem.styled.tsx
@@ -13,12 +13,22 @@ export const StyledPostListItem = styled.div`
     background-color: var(--primaryHoverFill);
     cursor: pointer;
   }
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+    padding: 0.5rem;
+    gap: 1rem;
+  }
 `;
 
 export const ImageContainer = styled.div`
   position: relative;
   height: 130px;
   aspect-ratio: 16/9;
+
+  @media (max-width: 800px) {
+    height: auto;
+  }
 `;
 
 export const Content = styled.div`
@@ -26,6 +36,11 @@ export const Content = styled.div`
   flex-direction: column;
   padding-top: 0.6rem;
   gap: 0.5rem;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+    padding: 0.3rem;
+  }
 `;
 
 export const Title = styled.h3`
@@ -37,6 +52,13 @@ export const Summary = styled.p`
   color: var(--tertiaryText);
   font-size: 14px;
   margin: 0px;
+
+  text-overflow: ellipsis;
+  overflow: hidden;
+  word-break: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 `;
 
 export const Date = styled.span`
--- src/widgets/RelatedPostList/ui/RelatedPostList.styled.tsx
+++ src/widgets/RelatedPostList/ui/RelatedPostList.styled.tsx
@@ -10,4 +10,13 @@ export const StyledRelatedPostList = styled.div`
   display: flex;
   gap: 1rem;
   flex-direction: column;
+
+  @media (max-width: 1400px) {
+    padding: 30px 1rem;
+    margin: 0px;
+  }
+
+  @media (max-width: 900px) {
+    width: 100%;
+  }
 `;
